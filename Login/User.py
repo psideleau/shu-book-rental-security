a77cb8257ccddb055ee10f66c735a3c14c4e5a9b
John Bucherati <jbucherati@gmail.com>
2018-11-02 18:15:08 -0400

    This is the user class definition

---
+++ Login/User.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self,username,password,email_address):
+        self.username = username
+        self.password = password
+        self.email_address = email_address
+        
