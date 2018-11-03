f507542bc37fce6e82024fc67183d1ad5398e682
John Bucherati <jbucherati@gmail.com>
2018-11-03 11:39:41 -0400

    updated user class file

--- Login/User.py
+++ Login/User.py
@@ -3,4 +3,12 @@ class User:
         self.username = username
         self.password = password
         self.email_address = email_address
-        
+    def showuser(self):
+         return self.username
+    def showpassword(self):
+         return self.password
+    def showemail(self):
+         return self.email_address
+
+#user1 = User("jbucherati","password1","jab140@snet.net")  
+#print(user1.showuser())
--- Login/login.py
+++ Login/login.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request,redirect,url_for
+from flask import Flask, render_template, request,redirect,url_for,jsonify
+from User import User
 app = Flask(__name__)
-
+user1 = User("jbucherati","password1","jab140@snet.net")
 @app.route('/')
 def home():
-   return "Welcome to SHU Book Rental App."
+   return jsonify({'username': user1.showuser(),
+    'email.address': user1.showemail()})
+
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
