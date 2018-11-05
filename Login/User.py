31c4e5782c2615a75bb4ff9efa3435276aa82d88
John Bucherati <jbucherati@gmail.com>
2018-11-05 18:29:39 -0500

    update for class

--- Login/User.py
+++ Login/User.py
@@ -9,6 +9,13 @@ class User:
          return self.password
     def showemail(self):
          return self.email_address
-
+    def resetpassword(self, newpassword):
+        self.password = newpassword
+        return self.password
 #user1 = User("jbucherati","password1","jab140@snet.net")  
 #print(user1.showuser())
+#(user1.showpassword())
+#user1.resetpassword('password2')
+#print(user1.showpassword())
+
+
--- Login/login.py
+++ Login/login.py
@@ -2,16 +2,33 @@ from flask import Flask, render_template, request,redirect,url_for,jsonify
 from User import User
 app = Flask(__name__)
 user1 = User("jbucherati","password1","jab140@snet.net")
+user2 = User("Mike","password2","mike@shu.edu")
+
 @app.route('/')
 def home():
-   return jsonify({'username': user1.showuser(),
-    'email.address': user1.showemail()})
+   return jsonify({'username': user1.showuser(),'email.address': user1.showemail()})
+   
 
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/login',methods = ['POST', "GET"])
 def login():
-    error = None
+  
     if request.method == 'POST':
-         return render_template('success.html')
+        
+        return render_template('success.html')
     return render_template('login.html')
-         
+
+@app.route('/showdata', methods = ['POST', 'GET'])
+def showdata():
+     user_login_name = request.form['username']
+     user_login_password = request.form['password']
+     
+     if user_login_name == user2.showuser():
+        return jsonify({'username': user2.showuser(),'email.address': user2.showemail()})
+     else:
+        return jsonify({'Logged in User': user_login_name, 'Password': user_login_password})
+   
+@app.route('/reset',methods = ['POST','GET'])
+def reset():
+    return render_template('reset.html')
+   
