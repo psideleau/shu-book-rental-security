139139e0c18e00af8ed92dba89bcb6cdd44ca119
John Bucherati <jbucherati@gmail.com>
2018-11-09 22:11:11 -0500

    Updated UserRepository Class

--- Login/User.py
+++ Login/User.py
@@ -1,21 +1,50 @@
-class User:
+from flask import jsonify
+
+class User():
+    
     def __init__(self,username,password,email_address):
         self.username = username
         self.password = password
         self.email_address = email_address
+     
     def showuser(self):
          return self.username
     def showpassword(self):
          return self.password
     def showemail(self):
          return self.email_address
-    def resetpassword(self, newpassword):
-        self.password = newpassword
-        return self.password
-#user1 = User("jbucherati","password1","jab140@snet.net")  
-#print(user1.showuser())
-#(user1.showpassword())
-#user1.resetpassword('password2')
-#print(user1.showpassword())
+    
+#user_list = []
+#user_list.append(User("jbucherati","Password1","jab140@snet.net"))
+#user_list.append(User("mike", "password2", "mike@shu.edu"))
+#user_list.append(User("abdullah", "password3", "abdullah@shu.edu"))
+
+#print(user_list[1].username)
+
+
+class UserRepository():
+    def __init__(self):
+          self.users = [User("jbucherati","Password1","jab140@snet.net"), User("mike", "password2", "mike@shu.edu"), User("abdullah", "password3", "abdullah@shu.edu")]
+    def listusers(self):
+        print(self.users)
+        self.users = tuple(self.users)
+        return self.users[0].username +" "+ self.users[1].username + " " + self.users[2].username
+    def search_user(self,user_search):
+       
+       self.user_search = user_search
+       if user_search == self.users[0].username:
+           return "User " + user_search + " Found"
+       elif user_search == self.users[1].username:
+           return "User " + user_search + " Found"
+       elif user_search == self.users[2].username:
+           return "User " + user_search + " Found"
+    
+       return user_search + "  User Not Found"
+    
+        
+
+
+    
+
 
 
--- Login/login.py
+++ Login/login.py
@@ -1,34 +1,59 @@
 from flask import Flask, render_template, request,redirect,url_for,jsonify
 from User import User
+from User import UserRepository
+import json
 app = Flask(__name__)
-user1 = User("jbucherati","password1","jab140@snet.net")
-user2 = User("Mike","password2","mike@shu.edu")
+User_repo = UserRepository()
+user_list = []
+user_list.append(User("jbucherati","Password1","jab140@snet.net"))
+user_list.append(User("mike", "password2", "mike@shu.edu"))
+user_list.append(User("abdullah", "password3", "abdullah@shu.edu"))
+#user1 = User("jbucherati","password1","jab140@snet.net")
+#user2 = User("Mike","password2","mike@shu.edu")
 
 @app.route('/')
 def home():
-   return jsonify({'username': user1.showuser(),'email.address': user1.showemail()})
+    return "Welcome to the SHU BookRental App"
+ #  return jsonify({'username': user1.showuser(),'email.address': user1.showemail()})
    
-
-
 @app.route('/login',methods = ['POST', "GET"])
 def login():
-  
-    if request.method == 'POST':
-        
-        return render_template('success.html')
-    return render_template('login.html')
+      return render_template('login.html')
+   
 
 @app.route('/showdata', methods = ['POST', 'GET'])
 def showdata():
      user_login_name = request.form['username']
      user_login_password = request.form['password']
      
-     if user_login_name == user2.showuser():
-        return jsonify({'username': user2.showuser(),'email.address': user2.showemail()})
+     if user_login_name == user_list[0].username:
+        return jsonify({'username': user_list[0].username,'email.address': user_list[0].email_address()})
      else:
         return jsonify({'Logged in User': user_login_name, 'Password': user_login_password})
-   
+
 @app.route('/reset',methods = ['POST','GET'])
 def reset():
     return render_template('reset.html')
+
+@app.route('/showusers', methods = ['POST', 'GET'])
+def showusers():
+     # User_repo = UserRepository()
+      return User_repo.listusers()
+    
+@app.route('/usersearch', methods =['POST', 'GET'])
+def usersearch():
+      
+      if request.method == "POST":
+            user_name = request.form['system_user']
+            return User_repo.search_user(user_name)
+      return render_template('usersearch.html')
    
+   
+   
+   
+   
+   
+    #jsonify({'Username':user_list[0].username, 'Password':user_list[0].password, 'email':user_list[0].email_address })
+
+
+
